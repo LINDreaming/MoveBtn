9c99aa111a90feef283dbfc1133ca6d188623e86
Linxi <zhaojuan@zhaojuans-MacBook-Air.local>
2015-01-05 14:43:46 +0800

    Initial Commit

---
+++ MoveBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoveBtn
+//
+//  Created by Linxi on 15/1/5.
+//  Copyright (c) 2015年 china08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
